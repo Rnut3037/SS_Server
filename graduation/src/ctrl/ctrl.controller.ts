1b9bd5e17d7058f6ef720b95b5ca1fda281486fb
Rnut3037 <pso303702@naver.com>
2024-10-20 15:41:09 +0900

    fall 코인 차감

--- graduation/src/ctrl/ctrl.controller.ts
+++ graduation/src/ctrl/ctrl.controller.ts
@@ -6,12 +6,12 @@ export class CtrlController {
     constructor(private ctrlService: CtrlService) {}
 
     @Post('control')
-    async controlCtrl(@Body() controlData: { dir: string }): Promise<string> {
-        const { dir } = controlData;
-        console.log(controlData);
+    async controlCtrl(@Body() controlData: { userId: string , dir: string}): Promise<string> {
+        const { userId,dir } = controlData;
+        console.log(controlData);   
 
         try {
-            await this.ctrlService.sendControlMessage(dir);
+            await this.ctrlService.sendControlMessage(dir,userId);
             return `Control message sent via MQTT: ${dir}`;
         } catch (error) {
             return `Error sending control message via MQTT: ${error.message}`;
--- graduation/src/ctrl/ctrl.module.ts
+++ graduation/src/ctrl/ctrl.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CtrlService } from './ctrl.service'; 
+import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/auth/user.entity';
 import { CtrlController } from './ctrl.controller';
-import { CtrlService } from './ctrl.service';
 
 @Module({
-  controllers: [CtrlController],
+  imports: [
+    TypeOrmModule.forFeature([User]), // 필요시 여기에 User 엔티티 등록
+    AuthModule, // AuthModule 임포트
+  ],
   providers: [CtrlService],
+  controllers: [CtrlController]
 })
 export class CtrlModule {}
